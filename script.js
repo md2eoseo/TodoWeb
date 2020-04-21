0ca05347e431f11c20fe5f7c6e882191f16e6f14
md2eoseo <md2eoseo@gmail.com>
2020-04-21 15:15:14 +0200

    added posting new card

--- script.js
+++ script.js
@@ -14,18 +14,82 @@ function showAddForm(e) {
   const saveBtn = document.createElement("button");
   const cancelBtn = document.createElement("button");
   const position = e.target.dataset.position;
+  form.id = "addForm";
   textarea.placeholder = "Write contents for this card...";
+  textarea.name = "desc";
   saveBtn.dataset.action = "inputTextarea";
   saveBtn.type = "submit";
   saveBtn.textContent = "Save";
-  saveBtn.dataset.position = position;
   saveBtn.dataset.action = "save";
+  form.setAttribute("novalidate", true);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkValidation(form, position);
+  });
   cancelBtn.textContent = "X";
   cancelBtn.dataset.action = "cancel";
   form.appendChild(textarea);
   form.appendChild(saveBtn);
   form.appendChild(cancelBtn);
   document.querySelector(`section#${position}`).appendChild(form);
-  document.querySelector(`.addBtn[data-position="${position}"]`).style.display =
-    "none";
+  document
+    .querySelector(`.addBtn[data-position="${position}"]`)
+    .classList.add("hidden");
+  form.elements.desc.focus();
+}
+
+function checkValidation(form, position) {
+  let validForm = false;
+  const elements = form.elements;
+
+  const textarea = form.querySelector("textarea");
+  textarea.classList.remove("invalid");
+  console.log(textarea.value);
+  if (!textarea.checkValidity()) {
+    ele.classList.add("invalid");
+  }
+
+  if (textarea.value != "") {
+    textarea.classList.remove("invalid");
+    // textarea_p.classList.add("hidden");
+    validForm = true;
+  } else {
+    route.classList.add("invalid");
+    // textarea_p.classList.remove("hidden");
+  }
+
+  if (form.checkValidity() && validForm) {
+    if (form == document.querySelector("#addForm")) {
+      post({ position: position, desc: textarea.value });
+      console.log("submitted " + elements.desc.value);
+      form.remove();
+      document
+        .querySelector(`.addBtn[data-position="${position}"]`)
+        .classList.remove("hidden");
+    } else {
+    }
+    return true;
+  } else {
+    if (!textarea.checkValidity()) {
+      ele.classList.add("invalid");
+    }
+    console.log("validation error!!");
+  }
+}
+
+function post(data) {
+  const postData = JSON.stringify(data);
+  fetch(DB_URL, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": API_KEY,
+      "cache-control": "no-cache",
+    },
+    body: postData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(`inserted ${data} in database`);
+    });
 }
